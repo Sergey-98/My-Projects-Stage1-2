df11c8780f29cc63e49d15163b9b4c39fa723d8a
Sergey-98 <yatsenko1998@yandex.ru>
2022-04-18 23:23:54 +0400

    feat: add popap to project

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,58 +1,56 @@
 `use strict`;
 
+import petsInfo from '../../js/pets.js';
 //--------------- for how help block--------------
-const help = {
-    0: {
+const help = [
+    {
         icon: '../../assets/icons/icon-pet-food.svg',
         title: 'Pet food',
     },
-    1: {
+    {
         icon: '../../assets/icons/icon-transportation.svg',
         title: 'Transportation',
     },
-    2: {
+    {
         icon: '../../assets/icons/icon-toys.svg',
         title: 'Toys',
     },
-    3: {
+    {
         icon: '../../assets/icons/icon-bowls-and-cups.svg',
         title: 'Bowls and cups',
     },
-    4: {
+    {
         icon: '../../assets/icons/icon-shampoos.svg',
         title: 'Shampoos',
     },
-    5: {
+    {
         icon: '../../assets/icons/icon-vitamins.svg',
         title: 'Vitamins',
     },
-    6: {
+    {
         icon: '../../assets/icons/icon-medicines.svg',
         title: 'Medicines',
     },
-    7: {
+    {
         icon: '../../assets/icons/icon-collars-leashes.svg',
         title: 'Collars / leashes',
     },
-    8: {
+    {
         icon: '../../assets/icons/icon-sleeping-area.svg',
         title: 'Sleeping areas',
     }
-
-}
-let helpLength = Object.keys(help).length;
-// let helpList = document.querySelector('.help-list');
-// const helpListItem = document.createElement('li');
-// helpListItem.classList.add('help-list-item');
-// helpList.append(helpListItem);
-// const iconItem = document.createElement('img');
-// iconItem.classList.add('help-list-item-icon');
-// iconItem.setAttribute(src, h.icon);
-// helpListItem.append(iconItem);
-
+];
+const hamburger = document.querySelector('.b-hamburger');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-link');
+let width = document.querySelector('body').offsetWidth;
 let helpList = document.querySelector('.help-list');
 
-for (let i = 0; i < helpLength; i++) {
+for (let i = 0; i < help.length; i++) {
+    createHelpList(i);
+}
+// -----------------------------------------------
+function createHelpList(i) {
     const helpListItem = document.createElement('li');
     helpListItem.classList.add('help-list-item');
     helpList.append(helpListItem);
@@ -66,16 +64,6 @@ for (let i = 0; i < helpLength; i++) {
     titleItem.textContent = help[i].title;
     helpListItem.append(titleItem);
 }
-// -----------------------------------------------
-
-
-const hamburger = document.querySelector('.b-hamburger');
-const nav = document.querySelector('.nav');
-const navList = document.querySelector('.nav-list');
-const navLinks = document.querySelectorAll('.nav-link');
-
-let width = document.querySelector('body').offsetWidth;
-
 function closeMenu(event) {
     if (event.target.classList.contains('nav-link')) { 
         hamburger.classList.remove('is-active');
@@ -100,10 +88,6 @@ if (width < 768) {
 } else if (width >= 768) {
     nav.classList.remove('none');
 }
-// if (width < 768) {
-//     nav.classList.add('none');
-//     console.log('none');
-// }
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     nav.classList.toggle('open');
@@ -114,3 +98,128 @@ if (width < 768) {
     }
   });
   navLinks.forEach((el) => el.addEventListener('click', closeMenu));
+
+// --------Card-------
+class Card {
+    constructor(src, alt, name, data) {
+        this.src = src;
+        this.alt = alt;
+        this.name = name;
+        this.data = data;
+        this.parent = document.querySelector('.slider');
+    }
+    renderCard() {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('data-pets', this.data);
+        card.innerHTML = `
+        <img src = ${this.src} alt = ${this.alt} class = "card-img" data-pets=${this.data}>
+        <h4 class = "card-title" data-pets=${this.data}>
+            ${this.name}
+        </h4>
+        <button class = "card-button" data-pets=${this.data}> 
+            Learn more
+        </button> 
+        `;
+        this.parent.append(card);
+    }
+}
+
+for (let i = 0; i < petsInfo.length-5; i++) {
+    new Card(
+            petsInfo[i].img,
+            petsInfo[i].alt,
+            petsInfo[i].name,
+            petsInfo[i].name,
+        ).renderCard();
+}
+const data = document.querySelectorAll('[data-pets]');
+//   ------Modal------
+class Modal {
+    constructor(name, src, alt, type, breed, description, age, inoculations, diseases, parasites) {
+        this.name = name;
+        this.src = src;
+        this.alt = alt;
+        this.type = type;
+        this.breed = breed;
+        this.description = description;
+        this.age = age;
+        this.inoculations = inoculations;
+        this.diseases = diseases;
+        this.parasites = parasites;
+    }
+
+    renderModal() {
+        let modalContent = document.querySelector('.modal-content');
+        modalContent.innerHTML = `
+                    <img src = ${this.src} class = "modal-photo" alt = ${this.alt}>
+                    <div class ="content-description">
+                        <div class = "desc-header">
+                            <h3 class = "description-name">
+                                ${this.name}
+                            </h3>
+                            <h4 class = "description-second-name">
+                                ${this.type} - ${this.breed}
+                            </h4>
+                        </div>
+                        <h5 class = "mod-desc">
+                            ${this.description}
+                        </h5>
+                        <ul class = "description-info">
+                            <li class = "info">
+                                <b class = "bold">Age:</b> <span class = "age">${this.age}</span>
+                            </li>
+                            <li class = "info">
+                                <b class = "bold">Inoculations:</b> <span class = "inoculations">${this.inoculations}</span>
+                            </li>
+                            <li class = "info">
+                                <b class = "bold">Diseases:</b> <span class = "diseases">${this.diseases}</span>
+                            </li>
+                            <li class = "info">
+                                <b class = "bold">Parasites:</b> <span class = "parasites">${this.parasites}</span>
+                            </li>
+                        </ul>
+                    </div>
+        `;
+    }
+}
+
+let modalWrapper = document.querySelector('.modal-wrapper');
+let modalContent = document.querySelector('.modal-content');
+let modalClose = document.querySelector('.modal-close');
+let modal = document.querySelector('.modal');
+
+
+function closeModal() {
+    modalWrapper.classList.add('none');
+    modalContent.innerHTML = ``;
+}
+
+modalWrapper.addEventListener('click', (event)=> {
+    if (event.target.classList.contains('modal-wrapper') || event.target.classList.contains('modal-close') || event.target.classList.contains('modal')) { 
+        closeModal();
+    }
+});
+
+function showModal(event) {
+    // modalWrapper.classList.remove('none');
+    modalWrapper.classList.remove('none');
+    const tar = event.target;
+    for (let i = 0; i < petsInfo.length; i++) {
+        if (petsInfo[i].name === tar.dataset.pets) {
+            new Modal(
+                petsInfo[i].name,
+                petsInfo[i].img,
+                petsInfo[i].alt,
+                petsInfo[i].type,
+                petsInfo[i].breed,
+                petsInfo[i].description,
+                petsInfo[i].age,
+                petsInfo[i].inoculations,
+                petsInfo[i].diseases,
+                petsInfo[i].parasites,
+            ).renderModal();
+        }
+    }
+}
+data.forEach((el) => el.addEventListener('click', (event) => showModal(event)));
