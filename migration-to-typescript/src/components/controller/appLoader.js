4afb1cdd940c86fd6a3b5ed1d517416492f7016d
Sergey-98 <yatsenko1998@yandex.ru>
2022-06-29 21:36:38 +0400

    feat: add Webpack and ESlint for typescript to project. Add tsconfig.json

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '50cc04cc811648eda863e5dcfd656fc2', // получите свой ключ https://newsapi.org/
         });
     }
 }
